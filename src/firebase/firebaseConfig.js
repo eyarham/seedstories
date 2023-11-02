98fa6bf077b26b8311477068253cb2eef8b7b238
Eric Yarham <eric.yarham@gmail.com>
2023-11-02 15:38:13 -0400

    adding home

---
+++ src/firebase/firebaseConfig.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDw_ixCaSuMnW6vVdzbJjDBw2n3_ftG7qI",
+  authDomain: "seedstories.firebaseapp.com",
+  projectId: "seedstories",
+  storageBucket: "seedstories.appspot.com",
+  messagingSenderId: "670325211826",
+  appId: "1:670325211826:web:1e62bcb3dee1731cc2bfab"
+};
+
+export default firebaseConfig;
