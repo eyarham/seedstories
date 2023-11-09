9d0bb6ff0385159b19589a3a855ddfe11368a6ce
Eric Yarham <eric.yarham@gmail.com>
2023-11-09 13:06:32 -0500

    add link to Header

--- src/_common/Header.jsx
+++ src/_common/Header.jsx
@@ -11,6 +11,9 @@ const Header = () => {
   const onClickBank = () => {
     navigate("/bank");
   }
+  const onClickNotebook = () => {
+    navigate("/notebook");
+  }
   return (
     <div style={{ backgroundColor: "#ddffdd", minHeight: "40px", padding: "5px" }}>
       <Container maxWidth="xl">
@@ -19,6 +22,9 @@ const Header = () => {
         </span>
         <span onClick={onClickBank} style={{ "float": 'left', fontSize: "20px", cursor: "pointer", marginLeft: "10px", color: "grey" }}>
           bank
+        </span>
+        <span onClick={onClickNotebook} style={{ "float": 'left', fontSize: "20px", cursor: "pointer", marginLeft: "10px", color: "grey" }}>
+          notebook
         </span>
         <span style={{ float: "right" }} >
           <AuthContainer />
